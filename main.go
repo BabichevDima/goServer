4d379eecea0eb2b40880f7be5700349d2b1140f8
Dmitry <dzmitry.babichev@gmail.com>
2025-07-10 19:01:36 +0300

    feat: add logs

--- main.go
+++ main.go
@@ -3,20 +3,51 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync/atomic"
+	"log"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	hits := cfg.fileserverHits.Load()
+	w.Write([]byte(fmt.Sprintf("Hits: %d", hits)))
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits reset to 0"))
+}
 
 func main() {
 	fmt.Println("Server started on localhost:8080")
 	mux := http.NewServeMux()
+	apiCfg := &apiConfig{}
 
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("./"))))
-	mux.Handle("/assets", http.FileServer(http.Dir("./")))
-	// mux.HandleFunc("/healthz", healthzHandler)
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("OK"))
-	})
+	// MetricsInc
+	fileServer := http.StripPrefix("/app/", http.FileServer(http.Dir("./")))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(fileServer))
+
+	// Not MetricsInc
+	mux.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./"))))
+
+	mux.HandleFunc("GET /healthz", healthzHandler)
+	mux.HandleFunc("GET /metrics", apiCfg.handlerMetrics)
+	// mux.Handle("GET /metrics", middlewareLog(http.HandlerFunc(apiCfg.handlerMetrics)))
+	mux.HandleFunc("POST /reset", apiCfg.handlerReset)
 
 	server := &http.Server{
 		Addr:    ":8080",
@@ -24,13 +55,21 @@ func main() {
 	}
 
 	err := server.ListenAndServe()
+
 	if err != nil {
 		panic(err)
 	}
 }
 
-// func healthzHandler (w http.ResponseWriter, r *http.Request) {
-// 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-// 		w.WriteHeader(http.StatusOK)
-// 		w.Write([]byte("OK"))
-// }
+func middlewareLog(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s", r.Method, r.URL.Path)
+		// next.ServeHTTP(w, r)
+	})
+}
+
+func healthzHandler (w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+}
