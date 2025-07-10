ef8a1eec7ecf4bcb4b7d8a2cca5f2b2baaa477d8
Dmitry <dzmitry.babichev@gmail.com>
2025-07-10 15:13:42 +0300

    feat: add handlers

--- main.go
+++ main.go
@@ -9,6 +9,14 @@ func main() {
 	fmt.Println("Server started on localhost:8080")
 	mux := http.NewServeMux()
 
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("./"))))
+	mux.Handle("/assets", http.FileServer(http.Dir("./")))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
@@ -18,6 +26,4 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
-	fmt.Println("Server started on localhost:8080")
 }
